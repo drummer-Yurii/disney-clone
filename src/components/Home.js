9fd98d9acb4baeb39aa07f5a563cbb6e8cf7489b
Yurii Rybachok <drummer2009@ukr.net>
2023-01-31 15:36:32 +0200

    add img slider component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import ImgSlider from './ImgSlider';
 
 const Home = () => {
   return (
     <Container>
-      Home
+      <ImgSlider />
     </Container>
   )
 }
@@ -15,6 +16,7 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
+  overflow-x: hidden;
 
   &:before {
     background: url("/images/home-background.png") center center / cover no-repeat fixed;
@@ -24,5 +26,6 @@ const Container = styled.main`
     left: 0;
     right: 0;
     bottom: 0;
+    z-index: -1;
   }
 `
