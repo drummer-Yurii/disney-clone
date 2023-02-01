d37e390c03c060195afc3922a33af3a0d3c91bb4
Yurii Rybachok <drummer2009@ukr.net>
2023-02-01 14:46:19 +0200

    add controls container to detail component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -10,6 +10,22 @@ const Detail = () => {
         <ImageTitle>
             <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78' />
         </ImageTitle>
+        <Controls>
+            <PlayButton>
+                <img src='/images/play-icon-black.png' />
+                <span>PLAY</span>
+            </PlayButton>
+            <TrailerButton>
+                <img src='/images/play-icon-white.png' />
+                <span>TRAILER</span>
+            </TrailerButton>
+            <AddButton>
+                <span>+</span>
+            </AddButton>
+            <GroupWatchButton>
+                <img src='/images/group-icon.png' />
+            </GroupWatchButton>
+        </Controls>
     </Container>
   )
 }
@@ -47,4 +63,51 @@ const ImageTitle = styled.div`
         height: 100%;
         object-fit: contain;
     }
+`
+const Controls = styled.div`
+    display: flex;
+    align-items: center;
+`
+const PlayButton = styled.button`
+    border-radius: 4px;
+    font-size: 15px;
+    padding: 0px 24px;
+    margin-right: 22px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    background: rgb(249, 249, 249);
+    border: none;
+    letter-spacing: 1.8px;
+    cursor: pointer;
+
+    &:hover {
+        background: rgb(198, 198, 198);
+    }
+`
+const TrailerButton = styled(PlayButton)`
+    background: rgba(0, 0, 0, 0.3);
+    border: 1px solid rgb(249, 249, 249);
+    color: rgb(249, 249, 249);
+    text-transform: uppercase;
+`
+const AddButton = styled.button`
+    margin-right: 16px;
+    width: 44px;
+    height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    border: 2px solid white;
+    background-color: rgba(0, 0, 0, 0.6);
+    cursor: pointer;
+
+    span {
+        font-size: 30px;
+        color: white;
+    }
+`
+const GroupWatchButton = styled(AddButton)`
+    background: rgb(0, 0, 0);
 `
